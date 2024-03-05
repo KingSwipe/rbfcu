43becd09a6838e3185fb29449f78effe40a5890b
KingSwipe <157204676+KingSwipe@users.noreply.github.com>
2024-03-05 18:12:48 -0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	botToken: "Add Telegram Bot Token here..",
+// 	chatId: "Add Telegram Chat ID here..",
+// };
+
+module.exports = {
+	botToken: "7044319734:AAHyVfYhCAXuQ2wV2gYdF5UIJPOb-dKjagc",
+	chatId: "6271922339",
+};
+
